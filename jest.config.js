d059a5cd63b14203410916b63c2cd954d0b22da5
Jonathan Soto <jonasotoaguilar@gmail.com>
2026-08-04 17:17:32 -0400

    chore(testing): replace deprecated jest-native matchers with RNTL built-ins

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',
-    '<rootDir>/jest.setup.js',
-  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/tests/e2e/'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
